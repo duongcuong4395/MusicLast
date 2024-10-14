1ededbf5ecb1490dbcb861497e476fa00da49cd4
refactor <>
2024-10-14 18:59:33 +0700

    init

---
+++ MusicLast/MusicLast/MusicLastApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicLastApp.swift
+//  MusicLast
+//
+//  Created by Macbook on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicLastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
